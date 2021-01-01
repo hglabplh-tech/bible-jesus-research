c4b796ce5af29bf32cf5318cc28e802c4064904e
Harald Glab-Plhak <harald.glab-plhak@t-online.de>
2021-01-01 14:05:18 +0100

    Managed to play audio for the whole german bible. Praise HIM

--- studier/src/main/java/org/harry/jesus/fxutils/media/PlayBible.java
+++ studier/src/main/java/org/harry/jesus/fxutils/media/PlayBible.java
@@ -7,6 +7,9 @@ import org.harry.jesus.jesajautils.BibleTextUtils;
 import org.pmw.tinylog.Logger;
 
 import java.io.File;
+import java.io.FilenameFilter;
+import java.util.List;
+import java.util.Optional;
 
 public class PlayBible {
 
@@ -31,24 +34,59 @@ public class PlayBible {
 
 
     public MediaPlayer  playChapter(BibleTextUtils.BookLink link) {
-        File chapterMP3 = getMP3File(link);
+        Optional<File> chapterMP3 = getMP3File(link);
         try {
-            if (chapterMP3.exists() && mView != null) {
-                Media hit = new Media(chapterMP3.toURI().toString());
+            if (chapterMP3.isPresent() && mView != null) {
+                Media hit = new Media(chapterMP3.get().toURI().toString());
                 mediaPlayer = new MediaPlayer(hit);
                 return mediaPlayer;
             }
         } catch (Exception ex) {
             Logger.trace(ex);
-            Logger.trace("Unable to play chapter: " + chapterMP3.getAbsolutePath() + "\nreasonCode:" + ex.getMessage());
+            Logger.trace("Unable to play chapter: "
+                    + chapterMP3.get().getAbsolutePath()
+                    + "\nreasonCode:" + ex.getMessage());
         }
         return mediaPlayer;
     }
 
-    public File getMP3File(BibleTextUtils.BookLink link) {
-        BibleTextUtils.BookLabel label  = new BibleTextUtils.BookLabel(link.getBookLabel());
-        String namePart = String.format("%s %d",label.getLongName(), link.getChapter());
-        String fullName = namePart + MP3_SUFFIX;
-        return new File(mediaPath, fullName);
+    public Optional<File> getMP3File(BibleTextUtils.BookLink link) {
+        File mediaDirFile= new File(mediaPath);
+        if (mediaDirFile.exists()) {
+            File [] files = mediaDirFile.listFiles(new MediaFilter(link));
+            if (files.length == 1) {
+                return Optional.of(files[0]);
+            }
+        }
+        return Optional.empty();
+    }
+
+    public static class MediaFilter implements FilenameFilter {
+
+        private final BibleTextUtils.BookLink link;
+
+        public MediaFilter(BibleTextUtils.BookLink link)  {
+            this.link = link;
+        }
+        @Override
+        public boolean accept(File dir, String name) {
+            BibleTextUtils.BookLabel label  = new BibleTextUtils.BookLabel(link.getBookLabel());
+            String namePart = String.format("%s %d",label.getLongName(), link.getChapter());
+            String bookChapterNo = name.substring(0, 5);
+            Integer bookNo = 0;
+            Integer chapterNo = 0;
+            if (bookChapterNo.matches("[0-9]+")) {
+                bookNo = Integer.parseInt(bookChapterNo.substring(0, 2));
+                chapterNo = Integer.parseInt(bookChapterNo.substring(2, 5));
+            }
+            if (name.contains(namePart)) {
+                return true;
+            } else if (label.getBookNumber().equals(bookNo)
+                    &&  link.getChapter().equals(chapterNo)) {
+                return true;
+            }
+            return false;
+        }
+
     }
 }
